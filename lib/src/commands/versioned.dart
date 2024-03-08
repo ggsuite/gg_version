bee4c6a427131d2ad6530059c5dc17e79f2d27b9
Gabriel Gatzsche <gabriel.gatzsche@grace.cloud>
2024-03-08 18:02:52 +0100

    Update dependencies

--- lib/src/commands/versioned.dart
+++ lib/src/commands/versioned.dart
@@ -8,7 +8,6 @@ import 'dart:io';
 
 import 'package:gg_console_colors/gg_console_colors.dart';
 import 'package:gg_git/gg_git.dart';
-import 'package:gg_is_github/gg_is_github.dart';
 import 'package:gg_process/gg_process.dart';
 import 'package:gg_status_printer/gg_status_printer.dart';
 import 'package:gg_version/gg_version.dart';
@@ -41,8 +40,7 @@ class Versioned extends GgGitBase {
 
     final printer = GgStatusPrinter<Version>(
       message: 'Check versions.',
-      printCallback: log,
-      useCarriageReturn: !isGitHub,
+      log: log,
     );
 
     try {
